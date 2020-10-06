02a27669d5a5981bd916a774c86c03566f58b873
LopatinaAnna <31049074+LopatinaAnna@users.noreply.github.com>
2020-10-06 23:53:59 +0300

    Add files

---
+++ testtask4/src/findvowels.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const findVowels = (str) => {
+    if (typeof(str) != 'string')
+        return 'Passed argument is not a string';
+
+    if (!str.length)
+        return 'String is empty';
+
+    let result = str.match(/[aeiou]/gi);
+
+    return result === null ? 'String does not contain vowels' : result.length;
+};
+
+module.exports = findVowels;
