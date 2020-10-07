1116bd2121fb8634ea7c6319f397d9afaabc7a42
LopatinaAnna <31049074+LopatinaAnna@users.noreply.github.com>
2020-10-07 12:00:05 +0300

    Add filter

--- testtask1/src/even-numbers.js
+++ testtask1/src/even-numbers.js
@@ -4,13 +4,9 @@ const evenNumbersInArray = (array) => {
     if (!Array.isArray(array) || !array.length)
         return 'Passed argument is not an array or empty';
 
-    let arr = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % 2 === 0)
-            arr.push(array[i]);
-    }
+    let result = array.filter((item) => item % 2 == 0);
 
-    return arr.length == 0 ? 'Passed array does not contain even numbers' : arr;
+    return result.length == 0 ? 'Passed array does not contain even numbers' : result;
 };
 
 module.exports = evenNumbersInArray;
